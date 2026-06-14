6dbf41cdeedf96bb952654fea4ffe49c72542541
HGhulam Hamza <khawajahamza566@gmail.com>
2026-06-14 13:46:26 +0500

    testing

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -29,7 +29,7 @@ function Home() {
       title: (
         <>
           ELEGANCE IN <br />
-          EVERY THREAD
+          EVERY SINGLE THREAD
         </>
       ),
       tagline: "Discover Our Exclusive Styles",
